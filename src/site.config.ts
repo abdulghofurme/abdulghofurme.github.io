99959f2b07186b0339fe3bf4e41bb964136297a1
Abdul Ghofur <abdulghofur.me@gmail.com>
2024-12-03 07:46:33 +0000

    change header name & page title

--- src/site.config.ts
+++ src/site.config.ts
@@ -4,7 +4,7 @@ export const siteConfig: SiteConfig = {
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
 	author: "Abdul Ghofur",
 	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
-	title: "abdulghofur.me",
+	title: "abdulghofurme",
 	// Meta property used as the default description meta property
 	description: "Abdul Ghofur - coder, runner & sleeper",
 	// HTML lang property, found in src/layouts/Base.astro L:18
