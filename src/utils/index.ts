d6f301df8170f06f414260d15d38d23debbec0f6
Abdul Ghofur <abdulghofur.me@gmail.com>
2024-12-26 14:59:54 +0700

    feature(comments): connect comment to devto

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,13 @@
 export { getFormattedDate } from "./date";
 export { elementHasClass, toggleClass } from "./domElement";
-export { getAllPosts, sortMDByDate, getUniqueTags, getUniqueTagsWithCount } from "./post";
+export {
+	getAllPosts,
+	sortMDByDate,
+	getUniqueTags,
+	getUniqueTagsWithCount,
+	parseCommentDevTo,
+	type Comment,
+	type DevToComment,
+} from "./post";
 export { generateToc } from "./generateToc";
 export type { TocItem } from "./generateToc";
